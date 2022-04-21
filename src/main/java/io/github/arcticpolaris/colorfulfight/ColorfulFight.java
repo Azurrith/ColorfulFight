5ade13ef5a19eafff9400f6d159202d3ba3407f9
Mengbill <2039433571@qq.com>
2022-04-21 23:03:58 +0800

    Init

---
+++ src/main/java/io/github/arcticpolaris/colorfulfight/ColorfulFight.java
@@ -0,0 +1,17 @@
+package io.github.arcticpolaris.colorfulfight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ColorfulFight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
